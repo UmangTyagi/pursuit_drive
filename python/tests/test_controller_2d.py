768e6d1df206f818b4885413d7b13acb497d1e6a
Umang Tyagi <umangtyagi1802@gmail.com>
2023-08-01 00:36:55 +0530

    adding test cases for other functions

--- python/tests/test_controller_2d.py
+++ python/tests/test_controller_2d.py
@@ -25,7 +25,7 @@ class Test_Controller_2d:
     controller is an object/insatnce of controller_2d class which is used to call its methods
     '''
     def setup_method(self):
-        self.waypoints = [[],[],[]]
+        self.waypoints = [waypoint(), waypoint(), waypoint()]
         self.long_vel = 0
         self.pose = mock_Pose()
         self.rotation = mock_Rotation()
@@ -122,3 +122,74 @@ class Test_Controller_2d:
         lookahead_distance = self.controller.calculate_lookahead_distance(rr_axle_pos_x,rr_axle_pos_y,self.next_waypoint)
 
         assert lookahead_distance == pytest.approx(expected_lookahead_distance)
+
+    '''
+    test cases for calculate_alpha
+    '''
+    def test_calculate_alpha_1(self):
+
+        expected_alpha = 0
+        alpha = self.controller.calculate_alpha(4,4)
+
+        assert alpha == pytest.approx(expected_alpha)
+
+    '''
+    test cases for calculate_req_str_angle
+    '''
+    def test_calculate_req_str_angle_1(self):
+
+        expected_str_angle = np.arctan2(2*4*np.sin(0.5), 4)
+        req_str_angle = self.controller.calculate_req_str_angle(4,0.5,1,4)
+
+        assert req_str_angle == pytest.approx(expected_str_angle)
+
+    '''
+    test cases for lateral controller
+    '''
+    def test_lateral_controlleR_1(self):
+
+        self.rotation.yaw = self.controller.wrap_to_pi(0.5)
+        self.pose.x = 10
+        self.pose.y = 10
+        self.wheelbase = 4
+        rr_axle_pose_x, rr_axle_pose_y = self.controller.calculate_rr_axle_pos(self.pose, self.wheelbase, self.rotation.yaw)
+        self.next_waypoint = waypoint()
+        self.next_waypoint.x = 0
+        self.next_waypoint.y = 0
+        lookahead_distance = self.controller.calculate_lookahead_distance(rr_axle_pose_x, rr_axle_pose_y, self.next_waypoint)
+        alpha = self.controller.calculate_alpha(self.wheelbase, lookahead_distance)
+        self.kpp = 1
+        self.controller.long_vel = 4
+        expected_str_angle  = self.controller.calculate_req_str_angle(self.wheelbase, alpha, self.kpp, 4)
+
+
+        req_str_angle = self.controller.lateral_controller(self.controller.wrap_to_pi, self.controller.calculate_rr_axle_pos, self.controller.calculate_lookahead_distance,
+                                                           self.controller.calculate_alpha, self.controller.calculate_req_str_angle)
+        
+        assert req_str_angle == pytest.approx(expected_str_angle)
+
+    '''
+    test cases for pid_controller
+    '''
+    def test_pid_controlleR(self):
+
+        long_vel_ref = 4.1
+        long_vel = 4
+        Kp = 1
+        Ki = 0.1
+        Kd = 0.01
+        E_k_1 = 1
+        e_k_1 = 0.02
+        dt = 0.05
+
+        e_k = long_vel_ref - long_vel
+        E_k = E_k_1 + e_k * dt
+        e_k_dot = (e_k - e_k_1)/dt
+        
+        expected_req_acc = Kp *e_k +  Ki * E_k +  Kd * e_k_dot
+        req_acc = self.controller.pid_controller(long_vel_ref, long_vel, Kp, Ki, Kd, E_k_1, e_k_1, dt)
+
+        assert req_acc == pytest.approx(expected_req_acc)
+
+
+ 
